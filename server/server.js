90203a9e3d9f061de4d8b9ae600382aecb8ac8cd
GhulamZaka <100795524+GhulamZaka@users.noreply.github.com>
2022-08-17 08:22:52 -0700

    adding reaction

--- server/server.js
+++ server/server.js
@@ -7,13 +7,16 @@ const {authMiddleware} = require('./utils/auth');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
+
+const app = express();
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
 });
 
-const app = express();
+
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -23,14 +26,13 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
   await server.start();
   server.applyMiddleware({ app });
+
 
   db.once('open', () => {
     app.listen(PORT, () => {
@@ -39,6 +41,11 @@ const startApolloServer = async (typeDefs, resolvers) => {
     })
   })
   };
+
   
   // Call the async function to start the server
   startApolloServer(typeDefs, resolvers);
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
